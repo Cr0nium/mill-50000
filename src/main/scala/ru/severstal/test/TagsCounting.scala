d59fba196b5ee7db49091943f1583e67eb50618f
Cr0nium <Cr0n4@mail.ru>
2019-06-12 15:24:55 +0300

    translated csv to DF

---
+++ src/main/scala/ru/severstal/test/TagsCounting.scala
@@ -0,0 +1,52 @@
+package ru.severstal.test
+
+import java.net.URL
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.types.{LongType, StringType, StructField, StructType, TimestampType}
+
+object TagsCounting extends App {
+
+  val sparkSession: SparkSession = SparkSession.builder
+    .master("local[*]")
+    .appName("Severstal_test")
+    .getOrCreate()
+
+  private var long_referance: URL = getClass.getClassLoader.getResource("long.csv")
+  private var rolls_referance: URL = getClass.getClassLoader.getResource("rolls.csv")
+
+  val schemaLong = StructType(List(
+    StructField("ts", TimestampType, nullable = true),
+    StructField("value", LongType, nullable = true),
+    StructField("tag", StringType, nullable = true)
+  ))
+
+  val schemaRolls = StructType(List(
+    StructField("roll_id", LongType, nullable = true),
+    StructField("roll_start", TimestampType, nullable = true),
+    StructField("roll_end", TimestampType, nullable = true)
+  ))
+
+  val longDF = sparkSession
+    .read
+    .option("header", "true")
+    .schema(schemaLong)
+    .option("delimiter",";")
+    .csv(long_referance.getPath)
+//  longDF.createOrReplaceGlobalTempView("long_view")
+
+  longDF.show()
+  longDF.printSchema()
+
+  val rollsDF = sparkSession
+    .read
+    .option("header", "true")
+    .schema(schemaRolls)
+    .option("delimiter",";")
+    .csv(rolls_referance.getPath)
+//  rollsDF.createOrReplaceGlobalTempView("long_view")
+
+  rollsDF.show()
+  rollsDF.printSchema()
+
+
+}
